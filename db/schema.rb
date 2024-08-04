2849362b45fa1d6710562fcaa922010453772c06
mhamza219 <mhamza219@gmail.com>
2024-08-04 14:26:22 +0530

    modal added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_04_065441) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_04_074224) do
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "subject"
+    t.float "marks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
